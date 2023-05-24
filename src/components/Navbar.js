adf4c9b7b9ea4e4e27c35eccb1691c15df6db924
Rajat <rajatkumar100095@gmail.com>
2023-05-25 02:10:48 +0530

    Hero section created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import '../App.css'
-import logo from '../images/logo.png'
+import Logo from '../images/Logo.png'
 
 function Navbar() {
   return (
     <div>
         <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
-            <a className="navbar-brand" href="/"><img src={logo} alt="logo" id='logo' /></a>
+            <a className="navbar-brand" href="/"><img src={Logo} alt="logo" id='logo' /></a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
